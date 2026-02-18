902c2c210cd7f1974388a41ffaca694982555260
Luth <luthsanches@gmail.com>
2026-02-18 16:21:32 -0300

    criacao do projeto
    criacao do projeto

---
+++ src/main/java/com/example/spring_4_java25/Spring4Java25Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_4_java25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4Java25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4Java25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_4_java25/Spring4Java25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_4_java25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring4Java25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
